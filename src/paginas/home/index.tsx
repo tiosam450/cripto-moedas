cd16567055966b4f18e08ea027dffb6eb69871c2
Samuel Nogueira <tiosam450@hotmail.com>
2024-07-31 21:33:53 -0400

    Desk Casa

--- src/paginas/home/index.tsx
+++ src/paginas/home/index.tsx
@@ -5,18 +5,54 @@ import { useEffect, useState } from "react";
 import { FormEvent } from "react";
 import s from './home.module.css'
 
+interface Props {
+    changePercent24Hr: string;
+    explorer: string;
+    id: string;
+    marketCapUsd: string;
+    maxSupply: string;
+    name: string;
+    priceUsd: string;
+    rank: string;
+    supply: string;
+    symbol: string;
+    volumeUsd24Hr: string;
+    vwap24Hr: string;
+    precoFormatado: string;
+    valorMercado: string;
+    volumeFormatado: string;
+}
+
+interface DataProps{
+    data: Props[]
+}
+
 export default function Home() {
     const [input, setInput] = useState<string | number>()
     const navigate = useNavigate()
-    const [coins, setCoins] = useState()
+    const [coins, setCoins] = useState<Props[]>([])
 
-    useEffect(()=>{
+    useEffect(() => {
         getData()
     }, [])
 
-    async function getData(){
-        fetch("https://api.coincap.io/v2/assets?limit=10&offset=0").then(resposta => resposta.json()).then((data:string[])=>{
-            console.log(data)
+    async function getData() {
+       await fetch("https://api.coincap.io/v2/assets?limit=10&offset=0").then(resposta => resposta.json()).then((data:DataProps) => {
+            const dataCoin = data.data
+
+            const formataPreco = Intl.NumberFormat('en-US',{style:'currency', currency:'USD'})
+            const formataPrecoCompacto = Intl.NumberFormat('en-US',{style:'currency', currency:'USD', notation:'compact'})
+
+            const moedaFormatada = dataCoin.map((item)=>{
+                const formato = {
+                    ...item,
+                    precoFormatado: formataPreco.format(Number(item.priceUsd)),
+                    valorMercado: formataPrecoCompacto.format(Number(item.marketCapUsd)),
+                    volumeFormatado: formataPrecoCompacto.format(Number(item.volumeUsd24Hr)),
+                }
+                return formato
+            })
+            setCoins(moedaFormatada)
         })
     }
 
@@ -27,9 +63,9 @@ export default function Home() {
 
     return (
         <section className={s.container}>
-            
+
             <Link to='/' className={s.logo}><img src={logo} alt="Logo" /></Link>
-            
+
             <form className={s.form} onSubmit={pesquisar}>
                 <input type="text" value={input} onChange={e => setInput(e.target.value)} />
                 <button><IoSearch /></button>
@@ -47,34 +83,37 @@ export default function Home() {
                 </thead>
 
                 <tbody>
-                    <tr className={s.tr}>
-                        <td data-label='Moeda:'>
-                            <div className="name">
-                                <Link to='/detalhes/bitcoin'><span>Bitcoin</span> | BTC</Link>
+                    {coins.length > 0 && coins.map((item)=>(
+                        <tr className={s.tr} key={item.id}>
+                        <td>
+                            <div className={s.nome}>
+                                <img className={s.iconeCoin} src={`https://assets.coincap.io/assets/icons/${item.symbol.toLowerCase()}@2x.png`} alt="" />
+                                <Link to={`/detalhes/${item.name}`}><span>{item.name} | {item.symbol}</span></Link>
                             </div>
                         </td>
 
                         <td data-label='Valor de mercado:'>
-                            Valor de mercado
+                            {item.valorMercado}
                         </td>
-                        
+
                         <td data-label='Preço:'>
-                            Preço
+                            {item.precoFormatado}
                         </td>
-                        
+
                         <td data-label='Volume:'>
-                            Volume
+                            {item.volumeFormatado}
                         </td>
-                        
-                        <td className={s.verde} data-label='Mudança 24h:'>
-                            Mudança 24h
+
+                        <td className={Number(item.changePercent24Hr) > 0 ? s.verde : s.verm} data-label='Mudança 24h:'>
+                            <span>{Number(item.changePercent24Hr).toFixed(2)}%</span>
                         </td>
                     </tr>
+                    ))}
                 </tbody>
             </table>
             <button className={s.btnMaisResultados}>Mais resultados</button>
 
         </section>
-        
+
     )
 }
